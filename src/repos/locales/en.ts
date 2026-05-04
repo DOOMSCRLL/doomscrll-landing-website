3c0bd82d2bd343f33b750d61126640b06bf2b0fe
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-05-05 01:30:14 +0300

    Write marketing text and localize it

--- src/repos/locales/en.ts
+++ src/repos/locales/en.ts
@@ -1,1 +1,20 @@
-export default {}
+export default {
+  meta: {
+    title: "DOOMSCRLL",
+    description: "DOOMSCRLL is a platform for showcasing independent projects for 24 hours.",
+  },
+  copy: "DOOMSCRLL is a platform for independents, created to let indie projects meet their audience. There is no algorithm, no noise. Your project is shown for 24 hours in a limited daily feed. You did the hard part, now show it to its audience.",
+  carousel: {
+    titleDefault: "DOOMSCRLL",
+    authorDefault: "_notymc",
+  },
+  appFunnel: {
+    copy: "Do you want to explore today's indie projects? Download the app, you don't need an account!",
+    cta: "Get the app",
+  },
+  doomlitFunnel: {
+    copy: "Are you an independent? Showcase your project for 24 hours! Reserve a DOOMLIT now.",
+    cta: "Reserve a DOOMLIT",
+    subtext: "Only for 1$!",
+  },
+}
--- src/repos/locales/tr.ts
+++ src/repos/locales/tr.ts
@@ -1,3 +1,22 @@
 import type { Dictionary } from "."
 
-export default {} satisfies Dictionary
+export default {
+  meta: {
+    title: "DOOMSCRLL",
+    description: "DOOMSCRLL, bağımsız projelerin 24 saatliğine sergilenebildiği bir sosyal ağdır.",
+  },
+  copy: "DOOMSCRLL bağımsızların projelerin kitlesini bulmaları için bir platformdur. Burada bir algoritma ya da gürültü yok. Projen 24 saatliğine kısıtlı bir günlük akışta sergilenir. Zor kısmı tamamladın, şimdi projeni kitlesine tanıt.",
+  carousel: {
+    titleDefault: "DOOMSCRLL",
+    authorDefault: "_notymc",
+  },
+  appFunnel: {
+    copy: "Bugünün bağımsız projelerine göz atmak ister misin? Uygulamayı indir, kullanmak için hesap gerekmiyor!",
+    cta: "Uygulamayı edin",
+  },
+  doomlitFunnel: {
+    copy: "Bir bağımsız mısın? Projeni 24 saatliğine sergileyebilirsin! Hemen bir DOOMLIT ayırt.",
+    cta: "Bir DOOMLIT ayırt",
+    subtext: "Yalnızca 1$!",
+  },
+} satisfies Dictionary
