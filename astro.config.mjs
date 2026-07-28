0f7223f9b02aaa48e81ce6b4a0f758dfd812b2e1
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-28 23:52:54 +0300

    chore: Add Cloudflare adapter and Wrangler to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
